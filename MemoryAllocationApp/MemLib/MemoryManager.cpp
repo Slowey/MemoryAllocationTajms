7241d89a6517bb2c6cab3e3fbca0bc8328185e97
slowey <lucas.holmqvist@gmail.com>
2016-09-12 15:03:05 +0200

    Made memory manager into a singleton

--- MemoryAllocationApp/MemLib/MemoryManager.cpp
+++ MemoryAllocationApp/MemLib/MemoryManager.cpp
@@ -1,7 +1,29 @@
 #include "MemoryManager.h"
 #include "Internal/PoolAllocatorInternal.h"
 
+
+MemoryManager* MemoryManager::m_singleton = nullptr;
+
+MemoryManager::MemoryManager()
+{
+}
+
+MemoryManager::~MemoryManager()
+{
+}
+
+
+MemoryManager * MemoryManager::Get()
+{
+    if (m_singleton == nullptr)
+        m_singleton = new MemoryManager();
+    return m_singleton;
+}
+
+
 PoolAllocator * MemoryManager::CreatePoolAllocator()
 {
     return new PoolAllocatorInternal();
 }
+
+
--- MemoryAllocationApp/MemoryAllocationApp/main.cpp
+++ MemoryAllocationApp/MemoryAllocationApp/main.cpp
@@ -3,14 +3,13 @@
 
 int main()
 {
-    // Create big ass memory manager (this should be a singleton)
-    MemoryManager memManager;
+
     // Create an allocator pointer
     PoolAllocator* poolAllocator;
     // Ask memory manager for an actual pool allocator
-    poolAllocator = memManager.CreatePoolAllocator();
+    poolAllocator = MemoryManager::Get()->CreatePoolAllocator();
     // Use pool allocator to varify it works
     int test = poolAllocator->TestMethod();
-    // Silly int. Put a breakpoint here to ensure the program doesn't end (there are better ways of doing this but cba)
+    //// Silly int. Put a breakpoint here to ensure the program doesn't end (there are better ways of doing this but cba)
     int stop = 2;
 }
