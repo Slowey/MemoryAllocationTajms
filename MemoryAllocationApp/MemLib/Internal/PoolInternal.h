4a4f11c408c1ef57da8267e880c5ee979adecacd
slowey <lucas.holmqvist@gmail.com>
2016-09-12 17:18:35 +0200

    Added pool class. It isn't really tested yet though

--- MemoryAllocationApp/MemLib/Internal/PoolInternal.h
+++ MemoryAllocationApp/MemLib/Internal/PoolInternal.h
@@ -1,7 +1,44 @@
 #pragma once
+#include <string>
+#include <queue>
 
+using namespace std;
 class PoolInternal 
 {
-    PoolInternal();
+    /**
+    Creates a pool of memory. Start of memory has to be provided by allocator.
+    p_segmentSize: how many bytes each segment is.
+    p_poolSize: total size (in bytes) of the pool*/
+    PoolInternal(void* p_memoryStart, int p_segmentSize, int p_poolSize);
     ~PoolInternal();
+
+    /**
+    Returns pointer to a free memory slot*/
+    void* Allocate();
+
+    /**
+    Returns if the pool is full*/
+    bool Full();
+
+private:
+    // Pointer to start of memory for this pool
+    void* m_memoryStart;
+
+    // Size of each segment within the pool
+    int m_segmentSize;
+
+    /** 
+    States whether the pool is full or not. This is used
+    by the pool allocator to know which pool memory should 
+    be allocated in*/
+    bool m_full;
+
+    // Index of last free index in memory pool
+    int m_lastFreeSegment;
+
+    // Number of segments in this pool
+    int m_numSegments;
+
+    // Queue of indices for empty segments
+    queue<int> m_emptySegments;
 };
