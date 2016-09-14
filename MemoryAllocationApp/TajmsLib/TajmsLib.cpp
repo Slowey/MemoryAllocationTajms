109589a7d38a25319ffdf0f00b5ac8154626ffe6
Yooric <yooric90@hotmail.com>
2016-09-14 11:37:02 +0200

    Lagt till funktionalitét som att den sparar ner dokumentet när man kallar på en funktion(ShutdownTajmsLib)
    Man kan spara undan timers men utan unikt id än så länge.

--- MemoryAllocationApp/TajmsLib/TajmsLib.cpp
+++ MemoryAllocationApp/TajmsLib/TajmsLib.cpp
@@ -1,5 +1,8 @@
 #include "TajmsLib.h"
 #include <ctime>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 //http://stackoverflow.com/questions/2808398/easily-measure-elapsed-time
 // kolla chrono grejen längre ner för clock kollade cpu clocken o inte actual time tydligen.
@@ -15,30 +18,59 @@
 void TajmsLib::InitTajmsLib()
 {
     //start the program timer.
-    unsigned int begin = clock();
-
-    unsigned int end = clock();
-    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    m_begin = clock();
 }
 void TajmsLib::ShutdownTajmsLib()
 {
     // Stop the timer and write to textfiles.
-    unsigned int end = clock();
+    // unsigned int end = clock();
+    time_t  timev = time(0);
+    time(&timev);
+    struct tm now;
+    localtime_s(&now, &timev);
+    int timeYear = now.tm_year + 1900;
+    std::string yearString = std::to_string(timeYear);
+
+    int timeMonth = now.tm_mon + 1;
+    std::string monthString = std::to_string(timeMonth);
+
+    int timeDay = now.tm_mday;
+    std::string dayString = std::to_string(timeDay);
+
+    int timeHour = now.tm_hour;
+    std::string hourString = std::to_string(timeHour);
+
+    int timeMinute = now.tm_min;
+    std::string minuteString = std::to_string(timeMinute);
+
+    int timeSecond = now.tm_sec;
+    std::string secondString = std::to_string(timeSecond);
+
+    std::ofstream myfile;
+    myfile.open(secondString + "_" + minuteString + "_" + hourString + "_" + dayString + "_" + monthString + "_" + yearString + ".txt");
+    myfile << timeSecond + timeMinute + timeHour + timeDay + timeMonth + timeYear;
+
+    myfile.close();
 }
 void TajmsLib::UpdateTimer()
 {
 
 }
-int TajmsLib::Test()
+float TajmsLib::Test()
 {
-    return 31;
+    float end = clock();
+    float elapsed_secs = double(end - m_begin) / CLOCKS_PER_SEC;
+    return elapsed_secs;
 }
-int TajmsLib::StartTimer()
+int TajmsLib::StartTimer(std::string p_timerName)
 {
-    return 0;
+    int timerId = 0; // Ändra tvåan till en funktion som fixar detta
+    m_timers.push_back(TajmsTimer(clock(), timerId, p_timerName));
+
+    return timerId;
 }
 void TajmsLib::StopTimer(int p_id)
 {
-
+    m_timers[p_id].timerEndTime = clock();
 }
 
