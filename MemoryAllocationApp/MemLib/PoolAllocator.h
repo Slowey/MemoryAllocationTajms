4ef787ca442a6eda8a66863c5245841c44a6303a
Akerspoket <konrad_o_92@hotmail.com>
2016-09-20 15:15:26 +0200

    Deallocation of memory in pool

--- MemoryAllocationApp/MemLib/PoolAllocator.h
+++ MemoryAllocationApp/MemLib/PoolAllocator.h
@@ -5,5 +5,5 @@ class PoolAllocator
 public:
     virtual int TestMethod() = 0;
     virtual void* Allocate() = 0;
-    virtual void Deallocate() = 0;
+    virtual void Deallocate(void* p_memory, const int& p_size) = 0;
 };
