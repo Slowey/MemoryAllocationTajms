acf273804c90b8c62a359f223aeed0d8c7c9cd7e
Eric <yooric90@hotmail.com>
2016-09-21 09:56:43 +0200

    Trådar kanske fungerar. Provat en del(i main) crashar inte

--- MemoryAllocationApp/MemoryAllocationApp/main.cpp
+++ MemoryAllocationApp/MemoryAllocationApp/main.cpp
@@ -25,27 +25,27 @@ public:
 //#define RUN_PRE_VALUES 1
 #define RUN_NORMAL 2
 #define TEST_TO_RUN 1
-std::mutex m;
-void task1(std::string msg)
-{
-	m.lock();
-	std::cout << "task1 says: " << msg << std::endl;
-	m.unlock();
-}
+//std::shared_ptr <std::mutex> m;
+//void task1(std::string msg)
+//{
+//	m->lock();
+//	std::cout << "task1 says: " << msg << std::endl;
+//	m->unlock();
+//}
 int main()
 {
     MemoryManager::Startup(1024, 100);
     TajmsLib tajm;
-    
-
+	//m = std::make_shared<std::mutex>();
+	//
 	//std::thread t1(task1, "Hello1");
 	//std::thread t2(task1, "Hello2");
 	//std::thread t4(task1, "Hello4");
 	//std::thread t5(task1, "Hello5");
-	//t1.join();
+	//t4.join();
 	//t2.join();
 	//t5.join();
-	//t4.join();
+	//t1.join();
 	//std::cout << "TRÅDAR SENSEI RAMEN FOOOOOOOOOOOOOOOOOO JOINADE";
 	//std::string hej2;
 	//std::cin >> hej2;
