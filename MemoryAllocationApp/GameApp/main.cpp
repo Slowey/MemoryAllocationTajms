9f27440a6de20d7c3b4fe118ff344c93a59e6353
Jawsarn <christian_markowicz@hotmail.com>
2016-10-07 14:57:53 +0200

    Added functions to FileLoader n' ZIPLoader

--- MemoryAllocationApp/GameApp/main.cpp
+++ MemoryAllocationApp/GameApp/main.cpp
@@ -5,5 +5,8 @@ int main()
     ResourceManager::Startup();
     ResourceManager* resMan = ResourceManager::Get();
 
+    std::string fileNAme = "test";
+    resMan->LoadChunk(fileNAme);
+
     int a = 3;
 }
--- MemoryAllocationApp/ResourceManagementLib/ResourceManager.h
+++ MemoryAllocationApp/ResourceManagementLib/ResourceManager.h
@@ -18,6 +18,9 @@ public:
 
 	void ReferencePing(/*GUID p_id*/);
 
+    // Temporary placeholder to test Loaders
+    void LoadChunk(std::string &p_fileName);
+
 private:
 	ResourceManager();
 	~ResourceManager();
