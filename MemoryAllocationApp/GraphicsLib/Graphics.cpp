103c530d8fe817bf9344bae7283bc77885813bbe
John Bergsten <putte_dt@hotmail.se>
2016-10-07 10:54:39 +0200

    Moved window creation to SDLManager

--- MemoryAllocationApp/GraphicsLib/Graphics.cpp
+++ MemoryAllocationApp/GraphicsLib/Graphics.cpp
@@ -1,7 +1,5 @@
 #include "Graphics.h"
 #include "Internal\SDLManager.h"
-#include <SDL\SDL.h>
-#include <GL2\glew.h>
 
 Graphics* Graphics::m_singleton = nullptr;
 
@@ -30,24 +28,6 @@ Graphics::~Graphics()
 
 void Graphics::CreateWindow(WindowParams p_parameters)
 {
-    SDL_Window* window;
-
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    window = SDL_CreateWindow(
-        p_parameters.windowName,
-        p_parameters.winPosX, 
-        p_parameters.winPosY, 
-        p_parameters.winSizeX,
-        p_parameters.winSizeY,
-        SDL_WINDOW_OPENGL);
-
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
-    GLenum status = glewInit();
-    if (status != GLEW_OK)
-        int derp = 5; // Just for debug purpose
-
-    // This has to be done somewhere
-    // SDL_DestroyWindow(window);
+    SDLManager::Get()->CreateWindow(p_parameters);
 
 }
--- MemoryAllocationApp/GraphicsLib/Internal/SDLManager.cpp
+++ MemoryAllocationApp/GraphicsLib/Internal/SDLManager.cpp
@@ -1,4 +1,12 @@
+// This class
 #include "SDLManager.h"
+
+// Third party
+#include <SDL\SDL.h>
+#include <GL2\glew.h>
+
+// Own stuff
+#include "../WindowParams.h"
 
 SDLManager* SDLManager::m_singleton = nullptr;
 
@@ -13,6 +21,25 @@ SDLManager::~SDLManager()
 void SDLManager::Startup()
 {
     m_singleton = new SDLManager();
+}
+
+void SDLManager::CreateWindow(WindowParams p_parameters)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    m_window = SDL_CreateWindow(
+        p_parameters.windowName,
+        p_parameters.winPosX,
+        p_parameters.winPosY,
+        p_parameters.winSizeX,
+        p_parameters.winSizeY,
+        SDL_WINDOW_OPENGL);
+
+    m_glContext = SDL_GL_CreateContext(m_window);
+    GLenum status = glewInit();
+    if (status != GLEW_OK)
+        int derp = 5; // Just for debug purpose. Should be done properly somewhere
+
 }
 
 
