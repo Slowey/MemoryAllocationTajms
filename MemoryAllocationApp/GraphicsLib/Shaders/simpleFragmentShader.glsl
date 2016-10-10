4381fe081d0429424b40bcdc9fe2a7eddca3e977
Azoooka <putte_dt@hotmail.se>
2016-10-10 16:52:12 +0200

    Updated shaders to use texture (outcommented in fragment shader since we haven't done the CPU part yet)

--- MemoryAllocationApp/GraphicsLib/Shaders/simpleFragmentShader.glsl
+++ MemoryAllocationApp/GraphicsLib/Shaders/simpleFragmentShader.glsl
@@ -1,7 +1,15 @@
 #version 430
 in vec4 posF;
+in vec2 texCoordF;
+
 out vec4 color;
+
+uniform sampler2D sampler;
+
 void main() 
 {
+	// Debug red color
 	color = vec4(1,0,0,0);
+	// Texture color
+	//color = textured2D(sample, texCoord);
 }
